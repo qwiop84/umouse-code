3ce944de79540fda4aafbe1e8da5f7da9f8c9b08
qwiop84 <102697408+qwiop84@users.noreply.github.com>
2022-03-23 22:50:12 +0000

    simple blinky program

--- my-project/my-project.c
+++ my-project/my-project.c
@@ -1,9 +1,17 @@
-#include "api.h"
-#include "api-asm.h"
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 
-int main(void) {
-	/* add your own code */
-	uint32_t rev = 0xaabbccdd;
-	rev = rev_bytes(rev);
-	return my_func(rev);
+int main(void)
+{
+	rcc_periph_clock_enable(RCC_LED1);
+	gpio_mode_setup(PORT_LED1, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED1);
+	gpio_set(PORT_LED1, PIN_LED1);
+	while(1) {
+		/* wait a little bit */
+		for (int i = 0; i < LITTLE_BIT; i++) {
+			__asm__("nop");
+		}
+		gpio_toggle(PORT_LED1, PIN_LED1);
+	}
+	return 0;
 }
